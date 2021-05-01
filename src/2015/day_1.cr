7168012b28cc403e97653cc7e90a711cd5e21722
Mafinar Khan <mafinar@coffeemeetsbagel.com>
2021-05-01 02:04:43 -0400

    Refactor String to Char

--- src/2015/day_1.cr
+++ src/2015/day_1.cr
@@ -5,25 +5,25 @@ module Year2015
     YEAR = 2015
     DAY  =    1
 
-    @data : Array(String)
+    @data : Array(Char)
 
     def initialize
       @data = read
     end
 
     def read
-      Common::Helper.read(YEAR, DAY).split("")
+      Common::Helper.read(YEAR, DAY).chars
     end
 
     def solve_1
-      @data.reduce(0) { |acc, i| i == "(" ? acc + 1 : acc - 1 }
+      @data.reduce(0) { |acc, i| i == '(' ? acc + 1 : acc - 1 }
     end
 
     def solve_2
       floor, idx = 0, 0
 
       while floor != -1
-        floor += @data[idx] == "(" ? 1 : -1
+        floor += @data[idx] == '(' ? 1 : -1
         idx += 1
       end
 
