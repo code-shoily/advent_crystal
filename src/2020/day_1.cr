637f0ce43fb9a1336b9618d507241050485d1f1d
Mafinar Khan <mafinar@coffeemeetsbagel.com>
2021-05-09 17:23:12 -0400

    Refactor

--- src/2020/day_1.cr
+++ src/2020/day_1.cr
@@ -5,39 +5,31 @@ module Year2020::Day1
     YEAR = 2020
     DAY  =    1
 
-    @data : Array(Int32)
+    @data = [] of Int32
+    @set : Set(Int32)
 
     def initialize
-      @data = process(Common::Helper.read(YEAR, DAY))
-    end
-
-    def initialize(input : String)
-      @data = process(input)
+      process(Common::Helper.read(YEAR, DAY))
+      @set = Set.new(@data)
     end
 
     def process(input : String)
-      input.split("\n").map { |i| i.to_i }
+      input.each_line { |line| @data << line.to_i }
     end
 
     def run_1
-      set = Set.new(@data)
-
       0.to(@data.size) do |idx|
-        other = 2020 - @data[idx]
-        return other * @data[idx] if set.includes?(other)
+        remaining = 2020 - @data[idx]
+        return remaining * @data[idx] if @set.includes?(remaining)
       end
     end
 
     def run_2
-      set = Set.new(@data)
-      @data
       0.to(@data.size - 1) do |idx|
-        should_total_to = 2020 - @data[idx]
-
         0.to(@data.size - 1) do |jdx|
           unless jdx == idx
-            other = should_total_to - @data[jdx]
-            return other * @data[idx] * @data[jdx] if set.includes?(other)
+            remaining = (2020 - @data[idx]) - @data[jdx]
+            return remaining * @data[idx] * @data[jdx] if @set.includes?(remaining)
           end
         end
       end
